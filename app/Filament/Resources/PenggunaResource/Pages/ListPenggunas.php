0c21951c542a9cc5f679f55de075ce2892322404
Nur Faidzin <nurfaidzin@student.janabadra.ac.id>
2024-07-14 20:51:07 +0700

    feat(ListPenggunas.php): add label to CreateAction in getHeaderActions() method to improve user experience

--- app/Filament/Resources/PenggunaResource/Pages/ListPenggunas.php
+++ app/Filament/Resources/PenggunaResource/Pages/ListPenggunas.php
@@ -13,7 +13,8 @@ class ListPenggunas extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()
+                        ->label('Buat Data Baru Pengguna'),
         ];
     }
 }
